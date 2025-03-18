4a5aaa445571a396e727c981a894e26764c773ac
Myourkiu <pedrofigsesteves@gmail.com>
2025-03-18 00:29:54 -0300

    feat/create user table
    - user model
    - entity base

---
+++ FoodSpot.Domain/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSpot.Domain
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+
+        public void Update()
+        {
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
--- FoodSpot.Infrastructure/ApplicationDbContext.cs
+++ FoodSpot.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FoodSpot.Domain.Models.Users;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace FoodSpot.Infrastructure
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
