09f0720fd38670d7e8e8fdaa5ef4028e6435c2ec
Myourkiu <pedrofigsesteves@gmail.com>
2025-03-19 12:30:06 -0300

    fix/entity base

--- FoodSpot.Domain/EntityBase.cs
+++ FoodSpot.Domain/EntityBase.cs
@@ -9,7 +9,7 @@ namespace FoodSpot.Domain
     public class EntityBase
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; }
 
         public void Update()
