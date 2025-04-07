d63009812ce1276ee2d9c9c26db1acdca1c78d83
Myourkiu <pedrofigsesteves@gmail.com>
2025-04-06 23:17:26 -0300

    feat/address repos

---
+++ FoodSpot.Infrastructure/Repositories/Implementation/Addesses/AddressRepository.cs
@@ -0,0 +1,40 @@
+﻿using FoodSpot.Domain.Models.Addresses;
+using FoodSpot.Domain.Models.Users;
+using FoodSpot.Infrastructure.Repositories.Interfaces.Addresses;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSpot.Infrastructure.Repositories.Implementation.Addesses
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public AddressRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Address> Create(Address address)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _context.Addresses.AddAsync(address);
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
+                    return address;
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+        }
+    }
+}
